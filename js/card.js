9c05b3bba84acad9b201c4cd404b18f84c0571ef
thagiuda <takahiro.hagiuda@gmail.com>
2023-02-17 15:57:25 +0900

    add card class

---
+++ js/card.js
@@ -0,0 +1,41 @@
+export default class Card {
+  // プロパティ
+  #rank;
+  #suit;
+  #index;
+
+  get rank() {
+    return this.#rank;
+  }
+
+  set rank(rank) {
+    this.#rank = rank;
+  }
+
+  get suit() {
+    return this.#suit;
+  }
+
+  set suit(suit) {
+    this.#suit = suit;
+  }
+
+  get index() {
+    return this.#index;
+  }
+
+  set index(index) {
+    this.#index = index;
+  }
+
+  constructor(index) {
+    this.rank = ((index - 1) % 13) + 1;
+    // 'A'はランク14として扱う
+    if (this.rank === 1) {
+      this.rank = 14;
+    }
+
+    this.suit = Math.floor((index - 1) / 13) + 1;
+    this.index = index;
+  }
+}
