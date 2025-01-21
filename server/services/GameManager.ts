818b8f71ea1e656a73d70185b5858b30340d1619
JauneBrillant <takahiro.hagiuda@gmail.com>
2025-01-22 02:56:26 +0900

    feat: ゲームの進行をつかさどるGameManagerクラスの実装(途中)

---
+++ server/services/GameManager.ts
@@ -0,0 +1,47 @@
+import { Round } from "@common/Round";
+import { Player } from "../models/Player";
+import { Deck } from "../models/Deck";
+import { GameState } from "@common/GameState";
+
+export class GameManager {
+  private deck = new Deck();
+  private state: GameState;
+
+  constructor(players: Player[]) {
+    this.state = {
+      players,
+      communityCards: [],
+      round: Round.PRE_FLOP,
+      pot: 0,
+      activePlayer: 0,
+    };
+  }
+
+  public getGameState(): GameState {
+    return this.state;
+  }
+
+  public startGame(): void {
+    this.deck.shuffle();
+    this.dealCards();
+    this.revealFlop();
+  }
+
+  private dealCards(): void {
+    for (const player of this.state.players as Player[]) {
+      player.setCards(this.deck.deal(2));
+    }
+  }
+
+  private revealFlop(): void {
+    this.state.communityCards = this.deck.deal(3);
+  }
+
+  private reveralTurn(): void {
+    this.state.communityCards.push(this.deck.deal(1)[0]);
+  }
+
+  private revealRiver(): void {
+    this.state.communityCards.push(this.deck.deal(1)[0]);
+  }
+}
