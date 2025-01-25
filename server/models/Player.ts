62eeca89b7d1471a8fae37b84eef15db02045065
JauneBrillant <takahiro.hagiuda@gmail.com>
2025-01-25 09:50:53 +0900

    feat: 型の変更に合わせてPlayerModelの修正

--- server/models/Player.ts
+++ server/models/Player.ts
@@ -1,9 +1,12 @@
-import { Card, Player as PlayerInterface } from "@common/types";
+import { Card, Player as PlayerInterface, TablePositions } from "@common/types";
 
 export class Player implements PlayerInterface {
   public name: string;
+  public position: TablePositions;
   public hand: Card[];
   public chips: number;
+  public currentbetAmount: number;
+  public isActive = true;
 
   constructor(name: string) {
     this.name = name;
@@ -15,6 +18,7 @@ export class Player implements PlayerInterface {
 
   public bet(amount: number): void {
     this.chips -= amount;
+    this.currentbetAmount += amount;
   }
 
   public addChips(amount: number): void {
